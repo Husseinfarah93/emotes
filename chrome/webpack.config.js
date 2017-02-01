b2dfeba4648a167673aae8565c2210f4b733c502
Hussein Farah <husseinfarah93@gmail.com>
2017-01-31 22:53:43 -0500

    set up webpack + dependancies + app structure

---
+++ chrome/webpack.config.js
@@ -0,0 +1,14 @@
+const webpack = require('webpack')
+
+module.exports = {
+    entry: "./src/app.jsx",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
