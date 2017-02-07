cbf01461fc80843f420a39a081c36853d7851990
Hussein Farah <husseinfarah93@gmail.com>
2017-02-07 13:57:52 -0500

    now changes all text on webpage to "rekt"

--- chrome/browser/bundle.js
+++ chrome/browser/bundle.js
@@ -44,9 +44,38 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	"use strict";
+	'use strict';
 
 	console.log("hello world");
+
+	var traverseDom = function traverseDom(node, funcToRun) {
+	  if (node.nodeType === 3 && node.nodeValue.trim() !== '') funcToRun(node);else node.childNodes.forEach(function (e) {
+	    return traverseDom(e, funcToRun);
+	  });
+	};
+
+	var rootNode = document.getElementsByTagName('body')[0];
+
+	var rektFunction = function rektFunction(elem) {
+	  var parent = elem.parentNode,
+	      newChild = document.createElement('text');
+	  newChild.textContent = 'rekt ';
+	  parent.replaceChild(newChild, elem);
+	};
+
+	traverseDom(rootNode, rektFunction);
+
+	/* 
+	Steps 
+	1) Replace all instances thing 
+	2) Add the typing mechanism
+	  2.1) Add listeners 
+	    2.1.1) If they submit an emote then re render
+	  2.2) If they type notifying thing display little box
+
+
+
+	*/
 
 /***/ }
 /******/ ]);
--- chrome/browser/entry.js
+++ chrome/browser/entry.js
@@ -1,4 +1,35 @@
 console.log("hello world")
 
 
+const traverseDom = (node, funcToRun) => {
+  if(node.nodeType === 3 && node.nodeValue.trim() !== '') funcToRun(node)
+  else node.childNodes.forEach(e => traverseDom(e,funcToRun))
+}
 
+let rootNode = document.getElementsByTagName('body')[0]
+
+
+
+const rektFunction = elem => {
+  let parent = elem.parentNode, newChild = document.createElement('text')
+  newChild.textContent = 'rekt '
+  parent.replaceChild(newChild,elem)
+}
+
+
+traverseDom(rootNode, rektFunction)
+
+
+
+
+/* 
+Steps 
+1) Replace all instances thing 
+2) Add the typing mechanism
+  2.1) Add listeners 
+    2.1.1) If they submit an emote then re render
+  2.2) If they type notifying thing display little box
+
+
+
+*/
